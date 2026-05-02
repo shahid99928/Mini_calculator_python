8771f7f2d5f031a184d88740810f83550782c52c
Shahid Abdul <shahid.abdul@haus.se>
2026-05-02 23:49:38 +0200

    Created basic math calculation functon and letting user to play around in a loop

---
+++ main.py
@@ -0,0 +1,27 @@
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        
+        else:
+            return "Error: Not divisible by zero"
+
+
+
+while True:
+    num1 = float(input("Enter your first number: "))
+    operator = input("Choose your operation (+,  -,  *,  /): ")
+    num2 = float(input("Enter your second number: "))
+    result = calculate(num1, num2, operator)
+    print(f"{num1}, {operator}, {num2} = {result}\n")
+    user_answer = input("do you want to continue? (y/n): ")
+    print("\n")
+    if user_answer == "n":
+        print("Goodbye!")
+        break
